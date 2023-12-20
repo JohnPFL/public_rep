36cb1d7e831c986ea13fd48a30af8f74e8fc39a3
JohnPFL <56122239+JohnPFL@users.noreply.github.com>
2023-12-20 21:13:41 +0100

    Add files via upload

---
+++ word_counter_bash.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+source /hpc/local/CentOS7/uu_cs_nlpsoc/conda/etc/profile.d/conda.sh 
+conda activate /hpc/local/CentOS7/uu_cs_nlpsoc/conda/envs/gs_env
+python3 /hpc/uu_cs_nlpsoc/sperduti/word_counter.py
