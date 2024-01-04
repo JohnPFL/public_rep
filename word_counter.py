edf2ca7795cf64aca7b19d7ea358f5f5c223dfe8
JohnPFL <56122239+JohnPFL@users.noreply.github.com>
2024-01-04 11:54:10 +0100

    Update word_counter.py

--- word_counter.py
+++ word_counter.py
@@ -57,5 +57,5 @@ class WordCounter:
 
 # Example usage:
 custom_output_path = 'custom_output_result.txt'
-word_counter = WordCounter('word_list_human_ev.txt', '/hpc/uu_cs_nlpsoc/data/coling2020_nguyen_grieve/data/twitter/london_tweets_processed_may2018_april2019_vocab_count.txt', custom_output_path)
+word_counter = WordCounter('word_list_human_ev.txt', '/hpc/uu_cs_nlpsoc/data/coling2020_nguyen_grieve/data/reddit/reddit_processed_2018_05_10_vocab_count.txt', custom_output_path)
 word_counter.process_datasets()
